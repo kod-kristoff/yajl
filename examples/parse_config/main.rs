a493628c00e870166df1659a1f6cca9d29b1c018
Kristoffer Andersson <kod.kristoff@gmail.com>
2023-04-28 19:48:12 +0000

    port example

--- examples/parse_config/main.rs
+++ examples/parse_config/main.rs
@@ -14,56 +14,60 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-#include "yajl/yajl_tree.h"
+use std::io;
+use std::io::Read;
 
-static unsigned char fileData[65536];
+use yajl::yajl_tree;
 
-int
-main(void)
-{
-    size_t rd;
-    yajl_val node;
-    char errbuf[1024];
+// static unsigned char fileData[65536];
+
+fn main() -> io::Result<()> {
+    // size_t rd;
+    // yajl_val node;
+    let mut fileData = [0; 65536];
+    let mut errbuf = [0; 1024];
 
     /* null plug buffers */
-    fileData[0] = errbuf[0] = 0;
-
+    // fileData[0] = errbuf[0c] = 0;
     /* read the entire config file */
-    rd = fread((void *) fileData, 1, sizeof(fileData) - 1, stdin);
+    // rd = fread((void *) fileData, 1, sizeof(fileData) - 1, stdin);
+    let rd = io::stdin().read(&mut fileData)?;
 
+    println!("read {rd}");
     /* file read error handling */
-    if (rd == 0 && !feof(stdin)) {
-        fprintf(stderr, "error encountered on file read\n");
-        return 1;
-    } else if (rd >= sizeof(fileData) - 1) {
-        fprintf(stderr, "config file too big\n");
-        return 1;
-    }
+    // if (rd == 0 && !feof(stdin)) {
+    //     fprintf(stderr, "error encountered on file read\n");
+    //     return 1;
+    // } else if (rd >= sizeof(fileData) - 1) {
+    //     fprintf(stderr, "config file too big\n");
+    //     return 1;
+    // }
 
     /* we have the whole config file in memory.  let's parse it ... */
-    node = yajl_tree_parse((const char *) fileData, errbuf, sizeof(errbuf));
+    // node = yajl_tree_parse((const char *) fileData, errbuf, sizeof(errbuf));
+    let node = yajl_tree::parse(&fileData, &mut errbuf);
 
     /* parse error handling */
-    if (node == NULL) {
-        fprintf(stderr, "parse_error: ");
-        if (strlen(errbuf)) fprintf(stderr, " %s", errbuf);
-        else fprintf(stderr, "unknown error");
-        fprintf(stderr, "\n");
-        return 1;
-    }
+    // if (node == NULL) {
+    //     fprintf(stderr, "parse_error: ");
+    //     if (strlen(errbuf)) fprintf(stderr, " %s", errbuf);
+    //     else fprintf(stderr, "unknown error");
+    //     fprintf(stderr, "\n");
+    //     return 1;
+    // }
 
     /* ... and extract a nested value from the config file */
-    {
-        const char * path[] = { "Logging", "timeFormat", (const char *) 0 };
-        yajl_val v = yajl_tree_get(node, path, yajl_t_string);
-        if (v) printf("%s/%s: %s\n", path[0], path[1], YAJL_GET_STRING(v));
-        else   printf("no such node: %s/%s\n", path[0], path[1]);
-    }
+    // {
+    //     const char * path[] = { "Logging", "timeFormat", (const char *) 0 };
+    //     yajl_val v = yajl_tree_get(node, path, yajl_t_string);
+    //     if (v) printf("%s/%s: %s\n", path[0], path[1], YAJL_GET_STRING(v));
+    //     else   printf("no such node: %s/%s\n", path[0], path[1]);
+    // }
 
-    yajl_tree_free(node);
+    // yajl_tree_free(node);
 
-    return 0;
+    Ok(())
 }
